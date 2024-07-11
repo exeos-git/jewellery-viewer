ebaab401440e1621b85243881af5137170b7d394
Tiffany Farthing <atifftoremember@gmail.com>
2024-07-11 16:07:49 +0100

    tried to ifx conflict again

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,6 @@ import {defineConfig, normalizePath} from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from 'path'
 
-// export default defineConfig({
-//     plugins: [
-//         viteStaticCopy({
-//             targets: [
-//                 {
-//                     src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
-//                     dest: normalizePath('./assets'), // 2️⃣
-//                 },
-//             ],
-//         }),
-//     ]
-// })
-
-
 export default defineConfig({
     plugins: [
         viteStaticCopy({
@@ -26,15 +12,8 @@ export default defineConfig({
                 },
             ],
         }),
-    ],
-    build: {
-        outDir: 'dist', // Ensure this is different from your source directory
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'index.html'),
-                editor: path.resolve(__dirname, 'editor.html')
-            }
-        }
-    }
-});
+    ]
+})
+
+
 
