6e321c13500a39927f567a1d35a6528cb0a9a75e
Tiffany Farthing <atifftoremember@gmail.com>
2024-07-12 09:19:48 +0100

    Updates

--- assets/main-6405ebf0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as i,b as _,c as s,d as n}from"./styles-aebcbdb9.js";async function t(){const a=new e({canvas:document.getElementById("webgi-canvas")});await i(a),await a.addPlugin(_),await a.addPlugin(s),await a.addPlugin(n),await a.load("./assets/Ring_Demo.glb")}t();
---
+++ assets/main-8b7c6669.js
@@ -0,0 +1,1 @@
+import{_ as e,a as i,b as s,c as _,d as n}from"./styles-aebcbdb9.js";async function t(){const a=new e({canvas:document.getElementById("webgi-canvas")});await i(a),await a.addPlugin(s),await a.addPlugin(_),await a.addPlugin(n),await a.load("./assets/classic-watch")}t();
