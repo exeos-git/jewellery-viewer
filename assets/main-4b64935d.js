3dfa255fb03415e4a12e948ece8f5a788af60513
Tiffany Farthing <atifftoremember@gmail.com>
2024-07-12 09:39:56 +0100

    Updates

--- assets/main-4b64935d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as s,b as i,c as _,d as t}from"./styles-aebcbdb9.js";async function n(){const a=new e({canvas:document.getElementById("webgi-canvas")});await s(a),await a.addPlugin(i),await a.addPlugin(_),await a.addPlugin(t),await a.load("./tree/gh-pages/assets/classic-watch.glb")}n();
