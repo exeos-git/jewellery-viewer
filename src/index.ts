264ffae7a7f5c1f4f238b9a78bb8a990204ad836
Tiffany Farthing <atifftoremember@gmail.com>
2024-08-27 08:11:33 +0100

    changed to model.glb

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ async function setupViewer(){
     await viewer.addPlugin(CanvasSnipperPlugin)
 
     // Import and add a GLB file.
-    await viewer.load("./assets/ring2.glb")
+    await viewer.load("./assets/model.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.setEnvironmentMap("./assets/environment.hdr");
